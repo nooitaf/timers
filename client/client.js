d25f25c17b4f74136bde2693be55a77e3c8d7d0b
nooitaf <nooitaf@gmail.com>
2015-10-11 19:18:45 +0200

    swtiched to chart

--- client/client.js
+++ client/client.js
@@ -1,42 +1,19 @@
+var testData = [{
+  value: 70,
+  color: "#FF0000"
+}, {
+  value: 30,
+  color: "#FFFFFF"
+}]
+
+
 Template.page.helpers({
-    topGenresChart : function() {
-        return {
-            chart: {
-                plotBackgroundColor: null,
-                plotBorderWidth: null,
-                plotShadow: false
-            },
-            title: {
-                text: this.username + "'s top genres"
-            },
-            tooltip: {
-                pointFormat: '<b>{point.percentage:.1f}%</b>'
-            },
-            plotOptions: {
-                pie: {
-                    allowPointSelect: true,
-                    cursor: 'pointer',
-                    dataLabels: {
-                        enabled: true,
-                        format: '<b>{point.name}</b>: {point.percentage:.1f} %',
-                        style: {
-                            color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
-                        },
-                        connectorColor: 'silver'
-                    }
-                }
-            },
-            series: [{
-                type: 'pie',
-                name: 'genre',
-                data: [
-                    ['Adventure',   45.0],
-                    ['Action',       26.8],
-                    ['Ecchi',   12.8],
-                    ['Comedy',    8.5],
-                    ['Yuri',     6.2]
-                ]
-            }]
-        }
-    }
+  pieData: function() {
+    return testData
+  }
+})
+
+Template.pie.onRendered(function() {
+  var ctx = this.find(".pie-canvas").getContext("2d")
+  var myPieChart = new Chart(ctx).Pie(this.data.pieData)
 })
